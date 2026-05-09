7b1f0fb276fa0e0b57f3f6abe1f2d98ddfe867f6
shunsei <k.shunsei1227@gmail.com>
2026-05-09 20:35:21 +0900

    fix login check path pattern

--- src/main/java/com/example/config/WebMvcConfig.java
+++ src/main/java/com/example/config/WebMvcConfig.java
@@ -32,7 +32,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor)
-                .addPathPatterns("/employees/list")
+                .addPathPatterns("/employees")
                 .addPathPatterns("/employees/detail");
     }
 }
